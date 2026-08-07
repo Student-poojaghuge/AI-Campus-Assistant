212f2632dba67a99ca207621fb032ceb36201e48
Pooja Ghuge <poojaghuge0510@gmail.com>
2026-08-07 20:29:45 +0530

    Day 3 - SQLite Database Setup

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -2,10 +2,8 @@ import sqlite3
 
 DATABASE_NAME = "database/users.db"
 
-
 def get_connection():
     return sqlite3.connect(DATABASE_NAME)
-
 
 def create_tables():
     conn = get_connection()
@@ -23,31 +21,3 @@ def create_tables():
 
     conn.commit()
     conn.close()
-
-
-# Insert New User
-def add_user(full_name, email, password, role="student"):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        INSERT INTO users(full_name, email, password, role)
-        VALUES (?, ?, ?, ?)
-    """, (full_name, email, password, role))
-
-    conn.commit()
-    conn.close()
-
-
-# Check Email Exists
-def email_exists(email):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM users WHERE email=?", (email,))
-
-    user = cursor.fetchone()
-
-    conn.close()
-
-    return user
